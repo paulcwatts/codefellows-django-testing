23f31629b367c96a6b9464f7f538d6d9256d3694
Paul Watts <paulcwatts@gmail.com>
2014-03-13 17:33:12 -0700

    Editing and deleting
    Edit and add use the same form class, so we need to add some extra
    context data to make the UI text make sense.

--- notes/urls.py
+++ notes/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, url
 
-from .views import NoteList, NoteCreate, NoteDetail
+from .views import NoteList, NoteCreate, NoteDetail, NoteUpdate, NoteDelete
 
 
 urlpatterns = patterns('notes.views',
     url(r'^$', NoteList.as_view(), name='note-list'),
     url(r'^(?P<pk>\d+)/$', NoteDetail.as_view(), name='note-detail'),
     url(r'^add/$', NoteCreate.as_view(), name='note-add'),
+    url(r'^(?P<pk>\d+)/edit/$', NoteUpdate.as_view(), name='note-update'),
+    url(r'^(?P<pk>\d+)/delete/$', NoteDelete.as_view(), name='note-delete'),
 )
--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import (CreateView, DeleteView,
+                                  DetailView, ListView, UpdateView)
 from django.core.urlresolvers import reverse_lazy
 
 from .forms import NoteForm
@@ -18,4 +19,26 @@ class NoteDetail(DetailView):
 class NoteCreate(CreateView):
     model = Note
     form_class = NoteForm
+    success_url = reverse_lazy('note-list')
+
+    def get_context_data(self, **kwargs):
+        context = super(NoteCreate, self).get_context_data(**kwargs)
+        context['title'] = 'Add note'
+        context['submit_text'] = 'Add'
+        return context
+
+
+class NoteUpdate(UpdateView):
+    model = Note
+    form_class = NoteForm
+
+    def get_context_data(self, **kwargs):
+        context = super(NoteUpdate, self).get_context_data(**kwargs)
+        context['title'] = 'Edit note'
+        context['submit_text'] = 'Save'
+        return context
+
+
+class NoteDelete(DeleteView):
+    model = Note
     success_url = reverse_lazy('note-list')
